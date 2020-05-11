fd347acf4ff04380f28e5620379ee38cebb4251a
Administrator <Administrator@192.168.105.231>
2020-05-11 22:54:39 +0800

    first commit

---
+++ src/com/xikan/controller/Student.java
@@ -0,0 +1,12 @@
+package com.xikan.controller;
+
+public class Student {
+	private String name;
+	private String hobby;
+	private int age;
+	
+	public void print(){
+		System.out.println(123);
+	}
+
+}
