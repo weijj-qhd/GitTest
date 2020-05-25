8ad65c2d5c56b937380e0cc7772e74b36e17af38
weijunjie <weijunjie1@guazi.com>
2020-05-25 20:49:08 +0800

    添加teacher文件

---
+++ src/com/xikan/Teacher.java
@@ -0,0 +1,16 @@
+package src.com.xikan;
+
+public class Teacher {
+	private String name;
+	private String hobby;
+	private int age;
+	
+	public void print() {
+		System.out.print(123);
+	}
+	
+	public int getint(){
+           System.out.print(22);
+	   return 2;
+	}
+}
